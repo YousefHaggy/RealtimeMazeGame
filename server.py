4cca66743a1eb6540f7e4f1e946edfc8a719de5b
Yousef <yousef.r.haggy@gmail.com>
2019-06-12 13:44:20 -0400

    more

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room
 from flask_cors import CORS
 import random
 app= Flask(__name__)
@@ -13,21 +13,30 @@ class Player:
 		self.row=row
 		self.playerID=playerID
 class Room:
-	def __init__(self,playerList,roomID):
-		self.playerList=playerList;
-		self.roomID=roomID;
-		seed=randint(1,100000)
-		while seed in roomList:
-			seed=randInt(1,100000)
+	def __init__(self):
+		self.playerList=[];
+		self.gameOver=False
+		self.seed=randint(1,100000)
 		roomList.add(seed)
+	def add_player(self,player):
+		self.playerList.append(player)
 @socketio.on('connect_to_queue')
 def handleConnect(data):
 	player=Player(0,0,random.randint(1,1000000))
-	if len(playersInQueue)>0:
-		startNewMatch()
-	else
-		playersInQueue.append(player)
+	matchFound=False
+	for room in roomList:
+		if len(room.playerList)<2:
+			matchFound=True
+			room.add_player(player)
+			join_room(room.seed)
+			startMatch(room.seed)
+	if not matchFound:
+		room=Room().add_player(player)
+		join_room(room.seed)
 	print(playersInQueue)
+def startNewMatch(seed):
+	send(seed,room=seed)
+
 
 
 
